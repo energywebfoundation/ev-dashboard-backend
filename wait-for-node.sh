88a4ed7b3eab21aebc4fc036a8194778c584a257
Adam Staveley <adam.staveley@energyweb.org>
2020-09-01 17:09:03 +0200

    change wait-for-node port to 8100

--- wait-for-node.sh
+++ wait-for-node.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-until $(curl --output /dev/null --silent --head --fail http://localhost:8080/health); do
+until $(curl --output /dev/null --silent --head --fail http://localhost:8100/health); do
     printf 'waiting for node up\n'
     sleep 5
 done
