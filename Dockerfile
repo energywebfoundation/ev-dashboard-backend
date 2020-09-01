fec1bc7219c4ac3146881650a7c4b0b2c1f965c3
Adam Staveley <adam.staveley@energyweb.org>
2020-09-01 11:33:59 +0200

    change hardcoded ocn config values to env vars; update dockerfile for ev-dashboard deployment

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Check out https://hub.docker.com/_/node to select a new base image
-FROM node:10-slim AS builder
+FROM node:lts-slim AS builder
 
 # Some dependencies from package.json are installed from git
 RUN apt-get update && apt-get upgrade -y && apt-get install -y git python3 build-essential
@@ -23,12 +23,13 @@ ENV NODE_ENV=production
 RUN npm run clean && npm run build
 
 # keep production dependencies only
-RUN rm -rf node_modules
-RUN npm install
+RUN npm prune --production
 
-FROM node:10-slim
+FROM node:lts-slim
 
 RUN apt-get update && apt-get upgrade -y
+RUN apt-get install curl -y
+
 # Set to a non-root built-in user `node`
 USER node
 
@@ -43,5 +44,9 @@ COPY --from=builder /home/node/app/index.js ./
 COPY --from=builder /home/node/app/dist ./dist
 COPY --from=builder /home/node/app/public ./public
 COPY --from=builder /home/node/app/node_modules ./node_modules
+COPY --from=builder --chown=node /home/node/app/inMemoryDB_EWFlex.json ./
+COPY --from=builder --chown=node /home/node/app/wait-for-node.sh ./
+
+RUN chmod +x ./wait-for-node.sh
 
 CMD ["node", "."]
--- index.js
+++ index.js
@@ -18,13 +18,13 @@ if (require.main === module) {
       host: process.env.HOST,
     },
     ocn: {
-      enabled: false,
+      enabled: process.env.OCN_ENABLED || true,
       // OCN environment: one of 'local', 'volta' or 'prod'
-      stage: 'local',
+      stage: process.env.OCN_STAGE || 'local',
       // URL as accessible by OCN node
-      bridgeURL: 'http://localhost:8090',
+      bridgeURL: process.env.OCN_BRIDGE_URL || 'http://localhost:8090',
       // URL as accessible by bridge
-      nodeURL: 'http://localhost:8100',
+      nodeURL: process.env.OCN_NODE_URL || 'http://localhost:8100',
       // The private key which registers Flex on the OCN; signs OCPI messages
       // note: stored in memory for convenience; will be replaced with key manager
       identity: process.env.OCN_IDENTITY,
@@ -32,9 +32,9 @@ if (require.main === module) {
       // Is invalidated after registration complete
       tokenA: process.env.OCN_TOKEN_A,
       // Configure MSPs to retrieve tokens from
-      msps: [{country_code: 'CH', party_id: 'MSP'}],
+      msps: [{country_code: 'DE', party_id: 'EVM'}],
       // Configure CPOs to retrieve locations from
-      cpos: [{country_code: 'CH', party_id: 'CPO'}],
+      cpos: [{country_code: 'DE', party_id: 'EVC'}],
     },
   };
   application.main(config).catch(err => {
